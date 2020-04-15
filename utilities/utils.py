89a14d0a8e92174a67380205126694593f6a590a
robzelluf <robzelluf@hotmail.com>
2020-04-15 23:43:16 +0300

    Add preprocess to utils

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,3 +1,12 @@
+from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_multiple_whitespaces, strip_punctuation
+
+
+def preprocess(lines):
+    for i, line in enumerate(lines):
+        lines[i] = preprocess_string(line, [lambda x: x.lower(), strip_tags, strip_multiple_whitespaces,
+                                            strip_punctuation])
+
+
 def language_map(lang):
     lang = lang.lower()
 
