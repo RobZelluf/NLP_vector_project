59385a4529566a4bf6be030551a93a74b4c0ed95
robzelluf <robzelluf@hotmail.com>
2020-04-16 02:08:57 +0300

    Update download script

--- download_script.py
+++ download_script.py
@@ -6,7 +6,7 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--skip_subtitles", type=bool)
-    parser.add_argument("--skip_embeddings" type=bool)
+    parser.add_argument("--skip_embeddings", type=bool)
 
     args = parser.parse_args()
 
