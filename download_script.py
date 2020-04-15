c271f080fecc3cbac993c7b7c3163a38694bfc50
robzelluf <robzelluf@hotmail.com>
2020-04-16 02:08:17 +0300

    Update download script

--- download_script.py
+++ download_script.py
@@ -1,8 +1,19 @@
 from utilities.download_utils.download_subtitles import download_all_subtitles
 from utilities.download_utils.download_vector_models import download_all_vector_models
+import argparse
 
-print("Downloading all subtitles")
-download_all_subtitles()
 
-print("Downloading all vector models")
-download_all_vector_models()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--skip_subtitles", type=bool)
+    parser.add_argument("--skip_embeddings" type=bool)
+
+    args = parser.parse_args()
+
+    if not args.skip_subtitles:
+        print("Downloading all subtitles")
+        download_all_subtitles()
+
+    if not args.skip_embeddings:
+        print("Downloading all vector models")
+        download_all_vector_models()
