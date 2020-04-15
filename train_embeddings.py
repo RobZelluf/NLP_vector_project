e85a8fac1c4c44044e884b61801e9362f46c3b3d
robzelluf <robzelluf@hotmail.com>
2020-04-16 02:17:00 +0300

    Properly open english subtitles

--- train_embeddings.py
+++ train_embeddings.py
@@ -10,10 +10,9 @@ import multiprocessing
 def get_num_lines(lang):
     lang_full, lang_short = language_map(lang)
 
-    if lang_short == "en":
-        filename = "subtitle_data.txt"
-    else:
-        filename = "OpenSubtitles.en-" + lang_short + "." + lang_short
+    filename = "OpenSubtitles.en"
+    if lang_short is not "en":
+        filename += "-" + lang_short + "." + lang_short
 
     file = "subtitle_data/" + lang_full + "/" + filename
     with open(file) as f:
