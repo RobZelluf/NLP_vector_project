25cec7b24c92832f1103d93ca8b9b5b3c7240ad0
robzelluf <robzelluf@hotmail.com>
2020-04-16 01:46:23 +0300

    Build embedding training file

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,4 +1,22 @@
 from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_multiple_whitespaces, strip_punctuation
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+
+def visualize_embeddings(model, words):
+    X = model[model.wv.vocab]
+
+    pca = PCA(n_components=2)
+    pca.fit(X)
+
+    X = model[words]
+    result = pca.transform(X)
+
+    plt.scatter(result[:, 0], result[:, 1])
+    for i, word in enumerate(words):
+        plt.annotate(word, xy=(result[i, 0], result[i, 1]))
+
+    plt.show()
 
 
 def preprocess(lines):
@@ -21,3 +39,40 @@ def language_map(lang):
 
     print("Warning: Language not detected, returning None")
     return None, None
+
+
+def load_subtitles(lang="nl", size=-1, start=None, end=None):
+    lang_full, lang_short = language_map(lang)
+
+    if lang_short == "en":
+        filename = "subtitle_data.txt"
+    else:
+        filename = "OpenSubtitles.en-" + lang_short + "." + lang_short
+
+    file = "subtitle_data/" + lang_full + "/" + filename
+    with open(file) as f:
+        if start is not None and end is not None:
+            subs = []
+            line = f.readline()
+            ind = 0
+            while line:
+                if start <= ind <= end:
+                    subs.append(line)
+
+                line = f.readline()
+                ind += 1
+                if ind > end:
+                    break
+
+            return subs
+
+        elif size == -1:
+            subs = f.read().splitlines()
+            return subs
+        else:
+            subs = []
+            for i in range(size):
+                sentence = f.readline()
+                subs.append(sentence)
+
+            return subs
