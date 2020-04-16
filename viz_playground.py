c547cbbec2a4b1c8481169f6dfad843e34386a4d
robzelluf <robzelluf@hotmail.com>
2020-04-16 15:38:55 +0300

    Improve playground

--- viz_playground.py
+++ viz_playground.py
@@ -1,7 +1,6 @@
 from utilities.utils import visualize_embeddings
 from gensim.models import Word2Vec
 
-
 model = Word2Vec.load("trained_models/dutch/nl_d300.model")
 model.init_sims(replace=True)
 
@@ -10,6 +9,6 @@ model.init_sims(replace=True)
 #          "one", "two", "three", "four", "adult"]
 
 words = ["aap", "hond", "kat", "koe", "auto", "fiets", "taxi", "vliegtuig", "trein",
-         "een", "twee", "drie", "vier", "vijf", "zesThe Dutch "]
+         "een", "twee", "drie", "vier", "vijf", "zes"]
 
 visualize_embeddings(model, words)
