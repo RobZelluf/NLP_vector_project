7f901b80826565c545e956920e8169f21c84a065
robzelluf <robzelluf@hotmail.com>
2020-04-18 14:16:17 +0300

    Allow FastText training

--- train_embeddings.py
+++ train_embeddings.py
@@ -1,4 +1,4 @@
-from gensim.models import Word2Vec
+from gensim.models import Word2Vec, FastText
 from utilities.utils import language_map, preprocess, load_subtitles,\
     get_num_lines, add_special_tokens, load_random_subtitles
 import logging
@@ -7,6 +7,66 @@ import random
 import math
 import multiprocessing
 import os
+
+
+def get_model_path(args):
+    lang_full, lang_short = language_map(args.language)
+    model_name = lang_short + "_d" + str(args.dim)
+    if args.fasttext:
+        model_name = "ft_" + model_name
+
+    if args.special_tokens:
+        model_name += "_st"
+    if args.skip_gram:
+        model_name += "_sg"
+    model_name += ".model"
+    model_path = "trained_models/" + lang_full + "/" + model_name
+    return model_name, model_path
+
+
+def set_sample_prob(args):
+    num_lines = get_num_lines(args.language)
+    print("Total number of lines:", num_lines)
+    chunk_size = int(num_lines / args.chunks)
+    if chunk_size > 5e6:
+        chunk_size = 5e6
+        args.chunks = int(math.ceil(num_lines / chunk_size))
+        print("Chunk size too large, set to", int(chunk_size), "with", args.chunks, "chunks!")
+    p = chunk_size / num_lines
+    return p
+
+
+def get_model(args, model_path):
+    cores = max(1, multiprocessing.cpu_count() - 2)
+    if os.path.exists(model_path) and args.continue_training:
+        print("Continuing training existing model")
+        if args.fasttext:
+            model = FastText(model_path)
+        else:
+            model = Word2Vec.load(model_path)
+    else:
+        if args.fasttext:
+            model = FastText(min_count=20,
+                             window=5,
+                             size=args.dim,
+                             alpha=0.03,
+                             min_alpha=0.0007,
+                             workers=cores,
+                             sample=6e-5,
+                             negative=20,
+                             sg=args.skip_gram)
+        else:
+            model = Word2Vec(min_count=20,
+                             window=5,
+                             size=args.dim,
+                             alpha=0.03,
+                             min_alpha=0.0007,
+                             workers=cores,
+                             sample=6e-5,
+                             negative=20,
+                             sg=args.skip_gram)
+    model.workers = cores
+    return model
 
 
 def train_chunk(model, language, p, epochs, special_tokens):
@@ -22,17 +82,7 @@ def train_chunk(model, language, p, epochs, special_tokens):
 
 def train(args):
     lang_full, lang_short = language_map(args.language)
-
-    model_name = lang_short + "_d" + str(args.dim)
-    if args.special_tokens:
-        model_name += "_st"
-
-    if args.skip_gram:
-        model_name += "_sg"
-
-    model_name += ".model"
-
-    model_path = "trained_models/" + lang_full + "/" + model_name
+    model_name, model_path = get_model_path(args)
 
     print("Training on", lang_full, "- Embedding size:", args.dim, "- Loops:", args.loops,
           "- Chunks:", args.chunks, "- Epochs:", args.epochs)
@@ -42,33 +92,8 @@ def train(args):
     else:
         print("Using CBOW")
 
-    cores = max(1, multiprocessing.cpu_count() - 2)
-
-    if os.path.exists(model_path) and args.continue_training:
-        print("Continuing training existing model")
-        model = Word2Vec.load(model_path)
-    else:
-        model = Word2Vec(min_count=20,
-                         window=5,
-                         size=args.dim,
-                         alpha=0.03,
-                         min_alpha=0.0007,
-                         workers=cores,
-                         sample=6e-5,
-                         negative=20,
-                         sg=args.skip_gram)
-
-    model.workers = cores
-
-    num_lines = get_num_lines(args.language)
-    print("Total number of lines:", num_lines)
-    chunk_size = int(num_lines / args.chunks)
-    if chunk_size > 5e6:
-        chunk_size = 5e6
-        args.chunks = int(math.ceil(num_lines / chunk_size))
-        print("Chunk size too large, set to", int(chunk_size), "with", args.chunks, "chunks!")
-
-    p = chunk_size / num_lines
+    model = get_model(args, model_path)
+    sample_p = set_sample_prob(args)
 
     for loop in range(args.loops):
         epochs = int(max(1, args.epochs - loop))
@@ -77,7 +102,7 @@ def train(args):
         for i, chunk in enumerate(chunk_list):
             print("Loop", loop + 1, "/", args.loops, "- Chunk, ", i + 1, "/", args.chunks, "- Epochs:", epochs)
 
-            train_chunk(model, args.language, p, epochs, args.special_tokens)
+            train_chunk(model, args.language, sample_p, epochs, args.special_tokens)
             model.save(model_path)
             print("Model saved as", model_name)
 
@@ -85,19 +110,25 @@ def train(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--language", type=str, help="Language to train", default="en")
-    parser.add_argument("--loops", type=int, help="Number of full loops over the corpus", default=1)
+    parser.add_argument("--loops", type=int, help="Number of full loops over the corpus", default=10)
     parser.add_argument("--chunks", type=int, help="Number of chunks to split corpus in", default=10)
     parser.add_argument("--epochs", type=int, help="Number of epochs per chunk", default=5)
-    parser.add_argument("--dim", type=int, help="Embedding dimension", default=300)
+    parser.add_argument("--dim", type=int, help="Embedding dimension", default=100)
     parser.add_argument("--log", type=bool, help="Pass if you want gensim to print logs")
     parser.add_argument("--continue_training", type=bool, default=True)
     parser.add_argument("--special_tokens", action='store_true')
     parser.add_argument("--skip_gram", action='store_true')
+    parser.add_argument("--fasttext", action='store_true')
     args = parser.parse_args()
 
     if args.log:
         print("Gensim will output logs!")
         logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
+
+    if args.fasttext:
+        print("Training FastText model")
+    else:
+        print("Training Word2Vec model")
 
     if not os.path.exists("trained_models"):
         os.mkdir("trained_models")
--- viz_playground.py
+++ viz_playground.py
@@ -1,14 +1,16 @@
 from utilities.utils import visualize_embeddings
 from gensim.models import Word2Vec
 
-model = Word2Vec.load("trained_models/dutch/nl_d100_st.model")
+model = Word2Vec.load("trained_models/english/en_d100_st.model")
 model.init_sims(replace=True)
 
-# words = ["car", "train", "bike", "taxi", "cab", "dog",
-#          "cat", "cow", "pig", "apple", "banana", "plane", "airplane",
-#          "one", "two", "three", "four", "adult"]
+words = ["monkey", "dog", "cat", "cow", "car", "bike", "taxi", "cab", "airplane", "plane", "train",
+         "one", "two", "three", "four", "five", "six",
+         "amsterdam", "london", "berlin", "rotterdam", "amsterdam",
+         "netherlands", "germany", "england"]
 
-words = ["aap", "hond", "kat", "koe", "auto", "fiets", "taxi", "vliegtuig", "trein",
-         "een", "twee", "drie", "vier", "vijf", "zes", ".", "<SOS>", "<EOS>"]
+# words = ["aap", "hond", "kat", "koe", "auto", "fiets", "taxi", "vliegtuig", "trein",
+#          "een", "twee", "drie", "vier", "vijf", "zes", ".", "<SOS>", "<EOS>",
+#          "amsterdam", "london", "berlin", "rotterdam", "manchester"]
 
 visualize_embeddings(model, words)
