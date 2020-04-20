a6dd154df477e96a256e5c673ca67af1572c384a
robzelluf <robzelluf@hotmail.com>
2020-04-20 22:00:41 +0300

    Add filter to split_subtitles

--- utilities/split_subtitles.py
+++ utilities/split_subtitles.py
@@ -1,13 +1,14 @@
 import random
-from utils import language_map, get_num_lines
+from utils import language_map, get_num_lines, keep_lines
 from time import time
 import os
+import argparse
 
 random.seed(91)
 save_interval = 1e6
 
 
-def split(lang, train=0.6, val=0.2, test=0.2):
+def split(lang, train=0.6, val=0.2, test=0.2, filter_lines=False):
     assert train + val + test == 1.0
 
     lang_full, lang_short = language_map(lang)
@@ -15,6 +16,10 @@ def split(lang, train=0.6, val=0.2, test=0.2):
 
     if not os.path.exists("data/train_data/en_" + lang_short):
         os.mkdir("data/train_data/en_" + lang_short)
+
+    if filter_lines:
+        if not os.path.exists("data/train_data/en_" + lang_short + "_filtered"):
+            os.mkdir("data/train_data/en_" + lang_short + "_filtered")
 
     path = "data/subtitle_data/en_" + lang_short + "/OpenSubtitles.en-" + lang_short + ".en"
     num_lines = get_num_lines(path)
@@ -43,6 +48,8 @@ def split(lang, train=0.6, val=0.2, test=0.2):
     path_to = "data/subtitle_data/en_" + lang_short + "/OpenSubtitles.en-" + lang_short + "." + lang_short
 
     # Make training data
+    saved_lines = 0
+    print("Start reading file")
     with open(path_en, "r") as file1, open(path_to) as file2:
         line_num = 0
         line1 = file1.readline()
@@ -65,6 +72,15 @@ def split(lang, train=0.6, val=0.2, test=0.2):
             if (line_num + 1) % save_interval == 0:
                 print(lang_full.capitalize() + " - Read", line_num + 1, "out of", num_lines, "lines.")
 
+            if filter_lines:
+                if not keep_lines(line1, line2):
+                    line_num += 1
+                    line1 = file1.readline()
+                    line2 = file2.readline()
+                    continue
+
+            saved_lines += 1
+
             if line_num == next_train_line:
                 train_lines1.append(line1)
                 train_lines2.append(line2)
@@ -85,10 +101,6 @@ def split(lang, train=0.6, val=0.2, test=0.2):
 
                 if test_lines:
                     next_test_line = test_lines.pop()
-
-            line_num += 1
-            line1 = file1.readline()
-            line2 = file2.readline()
 
             if len(train_lines1) >= save_interval:
                 save_train_lines("train", lang_short, train_lines1, train_lines2)
@@ -115,19 +127,36 @@ def split(lang, train=0.6, val=0.2, test=0.2):
         if test_lines1:
             save_train_lines("test", lang_short, train_lines1, train_lines2)
 
+        print("Saved", saved_lines / num_lines * 100, "% of the lines")
 
-def save_train_lines(set_type, lang_short, lines1, lines2):
+
+def save_train_lines(set_type, lang_short, lines1, lines2, filter=False):
     print("Saving " + set_type + " files")
-    with open("data/train_data/" + "en_" + lang_short + "/en_" + set_type + ".txt", "a") as new_file1:
+
+    if filter:
+        dir1 = "data/train_data/" + "en_" + lang_short + "_filtered/en_" + set_type + ".txt"
+        dir2 = "data/train_data/" + "en_" + lang_short + "_filtered/" + lang_short + "_" + set_type + ".txt"
+    else:
+        dir1 = "data/train_data/" + "en_" + lang_short + "/en_" + set_type + ".txt"
+        dir2 = "data/train_data/" + "en_" + lang_short + "/" + lang_short + "_" + set_type + ".txt"
+
+    with open(dir1, "a") as new_file1:
         new_file1.writelines(lines1)
 
-    with open("data/train_data/" + "en_" + lang_short + "/" + lang_short + "_" + set_type + ".txt", "a") as new_file2:
+    with open(dir2, "a") as new_file2:
         new_file2.writelines(lines2)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filter", action='store_true')
+    args = parser.parse_args()
+
+    if args.filter:
+        print("Filtering subtitles")
+
     for lang in ["dutch", "russian"]:
 
         t = time()
-        split(lang)
+        split(lang, filter_lines=args.filter)
         print('Splitting up everything took {} mins'.format(round((time() - t) / 60, 2)))
